d687b95069ff26c1e2a2175ab057ff783c59342e
CLAUDENIR-SANTOS <claudenir.concursos@gmail.com>
2019-11-28 12:29:53 -0200

    Versão Final

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,10 @@ export class HomeComponent implements OnInit {
   produtos;
   constructor() {
     this.produtos = [
-      new Produto('Caneta', 10.00),
-      new Produto('Caderno', 15.00),
-      new Produto('Caneca', 5.00),
-      new Produto('Carteira', 25.00)
+      new Produto('Caneta', 10.00,'http://encurtador.com.br/rvyKN'),
+      new Produto('Caderno', 15.00,'http://encurtador.com.br/acmU7'),
+      new Produto('Caneca', 5.00,'http://encurtador.com.br/rNPS3'),
+      new Produto('Carteira', 25.00,'')
     ]
    }
 
